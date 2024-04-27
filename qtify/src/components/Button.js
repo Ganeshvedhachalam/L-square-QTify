9c28fb948f1afee7ac0899141cc2f72ebcdfada0
Ganeshvedhachalam <ganeshgreat08@gmail.com>
2024-04-27 23:09:15 +0530

    font button name  changed

--- qtify/src/components/Button.js
+++ qtify/src/components/Button.js
@@ -3,14 +3,18 @@ import Button from "@mui/material/Button";
 function Buttonforproject (){
     return(
     <>
-    <Button className="custom-button"
-                        variant="outlined" 
-    sx={{ backgroundColor: 'black', color:"chartreuse",   border:"2px",
-    
-      }} 
-    >
-    Give feedback
-     </Button>
+   {/* <Button className="custom-button"
+          variant="outlined" 
+          sx={{ backgroundColor: 'black', color:"chartreuse", border:"2px",
+                // fontFamily: "Roboto, Helvetica, Arial, sans-serif, 'Poppins'"
+              }} 
+  >
+    Give Feedback
+  </Button> */}
+  <button className="custom-button"> 
+  Give Feedback
+
+  </button>
      </>
     )
 }
