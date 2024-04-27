c590bf4285be01f0e9613322d8c7b093d2148d81
Ganeshvedhachalam <ganeshgreat08@gmail.com>
2024-04-27 23:58:04 +0530

    new mui hover

--- qtify/src/components/Button.js
+++ qtify/src/components/Button.js
@@ -3,18 +3,18 @@ import Button from "@mui/material/Button";
 function Buttonforproject (){
     return(
     <>
-   {/* <Button className="custom-button"
-          variant="outlined" 
-          sx={{ backgroundColor: 'black', color:"chartreuse", border:"2px",
-                // fontFamily: "Roboto, Helvetica, Arial, sans-serif, 'Poppins'"
+   <Button className="custom-button"
+          // variant="outlined" 
+          sx={{ backgroundColor: 'black', color:" #34C94B", border:"2px",
+                fontFamily: 'Poppins'
               }} 
   >
     Give Feedback
-  </Button> */}
-  <button className="custom-button"> 
+  </Button>
+  {/* <button className="custom-button"> 
   Give Feedback
 
-  </button>
+  </button> */}
      </>
     )
 }
