23ff7402b39e8d9fad8b8e7d35f7f5a47a7f5c06
Ganeshvedhachalam <ganeshgreat08@gmail.com>
2024-04-27 13:40:15 +0530

    new poject modified

--- qtify/src/components/Navbar.js
+++ qtify/src/components/Navbar.js
@@ -1,22 +1,45 @@
 import React from 'react';
-import "./Navbar.css"
-
-
+import "./Navbar.css";
+import Button from "@mui/material/Button";
+import SearchIcon from "@mui/icons-material/Search";
+import SaveIcon from "@mui/icons-material/Save";
+import { useSnackbar } from 'notistack';
 
 function Navbar() {
+    const { enqueueSnackbar } = useSnackbar();
+   
     return (
-        <nav className="navbar">
+        <>
+         <nav className="navbar">
             <div className="container">
-                <a href="/" className="navbar-brand">My App</a>
-                <form className="navbar-form">
+
+                <a href="/" className="navbar-brand">
+                    <img src="https://th.bing.com/th?id=OIP.L1dgSDIjuIZAZvEAf5_cMgAAAA&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
+                     alt="Qtify Music" className="logo-image" />
+                </a>
+
+                
                     <div className="search-container">
                         <input type="text" className="form-control" placeholder="Search for a song" />
+
+                        <Button className="search-button">
+                            <SearchIcon />
+                        </Button>
                     </div>
                     
-                    <button type="submit" className="feedbackbtn">Give feedback</button>
-                </form>
+                    
+                    <Button 
+                        variant="outlined" 
+                        sx={{ backgroundColor: 'black', color:"chartreuse", '&:hover': { backgroundColor: 'chartreuse', color: 'black', } }} 
+                        className='muiButton' 
+                        startIcon={<SaveIcon/>}
+                    >
+                        Give feedback
+                    </Button>
+                
             </div>
         </nav>
+        </>
     );
 }
 
