1b58339d5f2816640d354a0b7bec5cd65b010a4c
Ganeshvedhachalam <ganeshgreat08@gmail.com>
2024-05-04 22:44:14 +0530

    songs section

---
+++ qtify/src/components/Albumcard/SongCard.js
@@ -0,0 +1,30 @@
+import { Card, CardContent, CardMedia, Chip, Typography } from "@mui/material";
+
+function SongCard({ album }) {
+  return (
+    <Card sx={{ maxWidth: 250, backgroundColor: "transparent", display: "flex", 
+    flexDirection: "column", alignItems: "center", marginBottom: 2 }}>
+      <CardMedia
+        component="img"
+        image={album.image}
+        alt={album.title}
+        sx={{ height: 300, width: 300, objectFit: 'cover' }}
+      />
+      <CardContent style={{padding:0 }}>
+      
+        <div style={{ display: "flex", justifyContent: "center",backgroundColor:"white" ,minWidth:"300px"}}>
+
+          <Chip color="primary" label={`${album.likes} likes`} size="large"
+           variant="filled" clickable style={{ width: 100, marginRight: 0 }} />
+           
+        </div>
+
+       
+      </CardContent>
+      <Typography gutterBottom variant="h6" component="div" color="white" align="center">
+          {album.title}
+        </Typography>
+    </Card>
+  );
+}
+ export default SongCard;
